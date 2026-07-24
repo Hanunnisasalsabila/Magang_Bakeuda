5aabd88e96be05ba97b82640e562ca2cad3cdd62
Aiskhazahwarayya <aiskharayya307@gmail.com>
2026-07-24 12:03:27 +0700

    feat: add oracle sync controller and implement village account management page

--- backend-bakeuda/reset_wilayah.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-async function main() {
-  await prisma.wilayah.deleteMany({});
-  console.log('All Wilayah deleted.');
-}
-
-main().finally(() => prisma.$disconnect());
