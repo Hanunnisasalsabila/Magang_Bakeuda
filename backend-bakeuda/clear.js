e0a33c10f36fb493fe46cbe170c8f1f8769cb97f
Afif Henr iSetiawan <afifhnristwn@gmail.com>
2026-08-05 17:39:37 +0700

    chore: remove legacy documentation and redundant SQL/CSV data files

--- backend-bakeuda/clear.js
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  await prisma.$executeRawUnsafe('DELETE FROM lampiran_dokumen');
-  console.log('Cleared');
-}
-main().finally(() => prisma.$disconnect());
--- backend-bakeuda/test_db.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-async function main() {
-  const op = await prisma.objekPajak.findUnique({
-    where: { nop: '330307001220000010' }
-  })
-  console.log(op)
-}
-main().finally(() => prisma.$disconnect())
